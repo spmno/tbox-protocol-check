e7289ebdf956342a9a3ba6d3db16f7190c161f09
sunqp <sunqp@meixing.com>
2019-06-28 18:02:38 +0800

    add environment for the protocol checker.

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+
+export default class App extends React.Component {
+  render() {
+    return (<div>
+      <h2>Welcome to React!</h2>
+    </div>);
+  }
+}
